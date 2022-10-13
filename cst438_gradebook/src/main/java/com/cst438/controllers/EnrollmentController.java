e3e531ec01ccae34a840d40261d37750bbe5c001
acuev <abcuevas@csumb.edu>
2022-10-13 11:34:07 -0700

    assignment 4 completed

--- cst438_gradebook/src/main/java/com/cst438/controllers/EnrollmentController.java
+++ cst438_gradebook/src/main/java/com/cst438/controllers/EnrollmentController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.cst438.domain.Assignment;
+import com.cst438.domain.AssignmentListDTO;
 import com.cst438.domain.Course;
 import com.cst438.domain.CourseRepository;
 import com.cst438.domain.Enrollment;
@@ -32,8 +34,24 @@ public class EnrollmentController {
 	public EnrollmentDTO addEnrollment(@RequestBody EnrollmentDTO enrollmentDTO) {
 		
 		//TODO  complete this method in homework 4
+		//Receive the EnrollmentDTO and update enrollment table.
+        Course c = courseRepository.findById(enrollmentDTO.course_id).orElse(null);
 		
-		return null;
+		if (c == null) {
+			throw new ResponseStatusException( HttpStatus.UNAUTHORIZED, "Course not found.");
+		}
+		//System.out.print(enrollmentDTO.course_id);
+		Enrollment enroll = new Enrollment();
+		enroll.setStudentEmail(enrollmentDTO.studentEmail);
+		enroll.setStudentName(enrollmentDTO.studentName);
+		enroll.setCourse(c);
+		
+		enroll = enrollmentRepository.save(enroll);
+		
+		//EnrollmentDTO result = new EnrollmentDTO(enrollmentDTO.studentEmail, enrollmentDTO.studentName, c.getCourse_id());		
+		enrollmentDTO.id = enroll.getId();
+		
+		return enrollmentDTO;
 		
 	}
 
--- cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceREST.java
+++ cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceREST.java
@@ -1,9 +1,12 @@
 package com.cst438.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 
 import com.cst438.domain.CourseDTOG;
+import com.cst438.domain.CourseDTOG.GradeDTO;
 
 public class RegistrationServiceREST extends RegistrationService {
 
@@ -19,8 +22,13 @@ public class RegistrationServiceREST extends RegistrationService {
 	
 	@Override
 	public void sendFinalGrades(int course_id , CourseDTOG courseDTO) { 
-		
 		//TODO  complete this method in homework 4
+		// When teacher does POST to /course/{course_id}/finalgrades 
+		// send final grades of all students to Registration backend using CourseDTOG
+		System.out.println("Sending final grade " + course_id + " "+ courseDTO);
+		restTemplate.put(registration_url+"/course/"+course_id, courseDTO);
+		System.out.println("After sending final grade");
+		
 		
 	}
 }
